660f4b53ea0371e836c2481752ce05aee170b60f
kinggordo <kinggordo@gmail.com>
2011-02-19 17:03:33 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kinggordo::Application.config.secret_token = '54df0e5a93b02ecf8d1b24c0654e2609f6cd51038ee0afa6f483f966cf1853e1d3ce9febb510cd5ce00f5e038f96cb3fd02028df3bcfcdec012b449d062f0689'
