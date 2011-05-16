f51facd6eb4ac5d1e1d4afbcc2e55630cb41300f
kinggordo <kinggordo@gmail.com>
2011-05-16 19:29:34 +0200

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,5 @@ Kinggordo::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'http://cold-mist-834.heroku.com/' }
 end
